ccaad069b13c4e4acbf9728d1b6af14a45ed79d0
WoosterInitiative <karlwooster@gmail.com>
2020-07-15 13:05:32 -0700

    some enhancements

--- src/extension.ts
+++ src/extension.ts
@@ -19,6 +19,9 @@ export function activate(context: vscode.ExtensionContext) {
 	let defaultContent = configuration.get('defaultContent');				// isn't used for anything, but is designed to allow for an alternate content location
 	let defaultSection = configuration.get('defaultSection');				// this is the "default" location for new posts
 	let defaultArchetype = configuration.get('defaultArchetype');			// this allows for a "default" archetype to be specified, option
+	let previewArguments = configuration.get('previewArguments');
+	let buildArguments = configuration.get('buildArguments');
+	let bindServer = configuration.get('bindServer');
 
 	// returns the hugo version being used
 	let getVersion = vscode.commands.registerCommand('turbohugo.version', () => {
@@ -35,81 +38,143 @@ export function activate(context: vscode.ExtensionContext) {
 		});
 	});
 
-	// this is the standard command for generating a new post based on section
-	let newPost = vscode.commands.registerCommand('turbohugo.newpost', () => {
-		vscode.window.showInputBox({ placeHolder: 'Section', value: `${defaultSection}` }).then((section) => {
-			if (section === undefined) {return;}
-			if (section === '') {
-				section = `${defaultSection}`;
-				vscode.window.showInformationMessage('Using default section \`' + defaultSection + '\`');
+	let newPost = vscode.commands.registerCommand('turbohugo.newpost', async () => {
+		let section = await vscode.window.showInputBox({ placeHolder: 'Section', value: `${defaultSection}` });
+		if (section === undefined) {return;}
+		if (section === '') {
+			section = `${defaultSection}`;
+			vscode.window.showInformationMessage('Using default sectin \`' + defaultSection + '\`');
+		}
+		let filename = await vscode.window.showInputBox({ placeHolder: 'Enter filename', prompt: 'Can include sub-folder' });
+		if (section === undefined) {return;}
+		if (filename === '') {
+			vscode.window.showWarningMessage('No filename specified, cancelling command.');
+			return;
+		}
+		
+		let postPath = section + '/' + filename;
+		let execCommand = 'hugo new ' + postPath + ' -s ' + vscode.workspace.rootPath;
+		exec(execCommand, (err: string, stdout: string, stderr: string) => {
+			if (stdout) {
+				vscode.window.showInformationMessage(stdout);
 			}
-			vscode.window.showInputBox({ placeHolder: 'Enter filename', prompt: 'Can include sub-folder' }).then((filename) => {
-				if (filename === undefined) {return;}
-				if (filename === '') {
-					vscode.window.showWarningMessage('No filename specified, cancelling turboHugo: New Post.');
-					return;
-				}
-				let postPath = section + '/' + filename;
-				let execCommand = 'hugo new ' + postPath + ' -s ' + vscode.workspace.rootPath;
-				exec(execCommand, (err: string, stdout: string, stderr: string) => {
-					if (stdout) {
-						vscode.window.showInformationMessage(stdout);
-					}
-					if (stderr) {
-						vscode.window.showWarningMessage(stderr);
-					}
-					if (err) {
-						vscode.window.showErrorMessage(err);
-					}
-				});
-			});
+			if (stderr) {
+				vscode.window.showWarningMessage(stderr);
+			}
+			if (err) {
+				vscode.window.showErrorMessage(err);
+			}
 		});
 	});
 
-	// slightly more advanced option that takes an archetype argument
-	let newArchetype = vscode.commands.registerCommand('turbohugo.newarchetype', () => {
-		vscode.window.showInputBox({ placeHolder: 'Section', value: `${defaultSection}` }).then((section) => {
-			if (section === undefined) {return;}
-			if (section === '') {
-				section = `${defaultSection}`;
-				vscode.window.showInformationMessage('Using default section \`' + defaultSection + '\`');
+	let newArchetype = vscode.commands.registerCommand('turbohugo.newarchetype', async () => {
+		let archetype = await vscode.window.showInputBox({ placeHolder: 'Archetype/Bundle', value: `${defaultArchetype}` });
+		if (archetype === undefined) {return;};
+		if (archetype === '') {
+			vscode.window.showWarningMessage('No archetype specified, try turboHugo: New Post instead.');
+			return;
+		}
+		let section = await vscode.window.showInputBox({ placeHolder: 'Section', value: `${defaultSection}` });
+		if (section === undefined) {return;}
+		if (section === '') {
+			section = `${defaultSection}`;
+			vscode.window.showInformationMessage('Using default section \`' + defaultSection + '\`');
+		}
+		let filename = await vscode.window.showInputBox({ placeHolder: 'Enter filename', prompt: 'Can include sub-folder' });
+		if (filename === undefined) {return;}
+		if (filename === '') {
+			vscode.window.showWarningMessage('No filename specified, cancelling command.');
+			return;
+		}
+
+		let postPath = section + '/' + filename;
+		let execCommand = 'hugo new --kind ' + archetype + ' ' + postPath + ' -s ' + vscode.workspace.rootPath;
+		exec(execCommand, (err: string, stdout: string, stderr: string) => {
+			if (stdout) {
+				vscode.window.showInformationMessage(stdout);
 			}
-			vscode.window.showInputBox({ placeHolder: 'Archetype/Bundle', value: `${defaultArchetype}` }).then((archetype) => {
-				if (archetype === undefined) {return;}
-				if (archetype === '') {
-					vscode.window.showWarningMessage('No archetype specified, try turboHugo: New Post instead.');
-					return;
-				}
-				vscode.window.showInputBox({ placeHolder: 'Enter filename', prompt: 'Can include sub-folder' }).then((filename) => {
-					if (filename === undefined) {return;}
-					if (filename === '') {
-						vscode.window.showWarningMessage('No filename specified, cancelling turboHugo: New Post.');
-						return;
-					}
-					let postPath = section + '/' + filename;
-					let execCommand = 'hugo new --kind ' + archetype + ' ' + postPath + ' -s ' + vscode.workspace.rootPath;
-					exec(execCommand, (err: string, stdout: string, stderr: string) => {
-						if (stdout) {
-							vscode.window.showInformationMessage(stdout);
-						}
-						if (stderr) {
-							vscode.window.showWarningMessage(stderr);
-						}
-						if (err) {
-							vscode.window.showErrorMessage(err);
-						}
-					});
-				});
-			});
+			if (stderr) {
+				vscode.window.showWarningMessage(stderr);
+			}
+			if (err) {
+				vscode.window.showErrorMessage(err);
+			}
 		});
 	});
-	
-	let buildPreview = vscode.registerCommand('buildPreview')
+
+	// runs hugo server with preview options
+	let buildPreview = vscode.commands.registerCommand('turbohugo.buildpreview', () => {
+		let terminal = vscode.window.createTerminal({
+			name: 'Hugo Server'
+		});
+
+		let execCommand = 'hugo server ' + previewArguments + ' -s ' + vscode.workspace.rootPath;
+		terminal.sendText( execCommand );
+
+		terminal.show;
+		/*
+		
+		exec(execCommand, (err: string, stdout: string, stderr: string) => {
+			if (stdout) {
+				vscode.window.showInformationMessage(stdout);
+			}
+			if (stderr) {
+				console.error('stderr:', stderr);
+			}
+			if (err) {
+				vscode.window.showErrorMessage(err);
+			}
+		});
+		*/
+	});
+/*
+	let buildPreview = vscode.commands.registerCommand('turbohugo.buildpreview', () => {
+		let terminal = vscode.window.createTerminal({
+			name: 'Hugo Server'
+		});
+
+		let execCommand = 'hugo server ' + previewArguments + ' -s ' + vscode.workspace.rootPath;
+		terminal.sendText( execCommand );
+
+		terminal.show;
+		/*
+		
+		exec(execCommand, (err: string, stdout: string, stderr: string) => {
+			if (stdout) {
+				vscode.window.showInformationMessage(stdout);
+			}
+			if (stderr) {
+				console.error('stderr:', stderr);
+			}
+			if (err) {
+				vscode.window.showErrorMessage(err);
+			}
+		});
+		*/
+	});
 
 	context.subscriptions.push(getVersion);
 	context.subscriptions.push(newPost);
 	context.subscriptions.push(newArchetype);
+	context.subscriptions.push(buildPreview);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+
+function selectTerminal(): Thenable<vscode.Terminal | undefined> {
+	interface TerminalQuickPickItem extends vscode.QuickPickItem {
+		terminal: vscode.Terminal;
+	}
+	const terminals = <vscode.Terminal[]>(<any>vscode.window).terminals;
+	const items: TerminalQuickPickItem[] = terminals.map(t => {
+		return {
+			label: `name: ${t.name}`,
+			terminal: t
+		};
+	});
+	return vscode.window.showQuickPick(items).then(item => {
+		return item ? item.terminal : undefined;
+	});
+}
